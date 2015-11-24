b2f6c92f2cb54de871def5bedd2544f89d32768a
Maxim Oransky <maxim.oransky@gmail.com>
2015-11-24 19:26:42 +0300

    readme

--- _vkscriptz.py
+++ _vkscriptz.py
@@ -57,23 +57,6 @@ USER_FIELDS = (
 )
 
 
-def vk_create_target_group(account_id, name, domain='', lifetime=0):
-    """
-    https://vk.com/dev/ads.createTargetGroup
-    """
-    resp = requests.get('https://api.vk.com/method/ads.createTargetGroup', params=dict(
-        account_id=account_id,
-        name=name,
-        domain=domain,
-        lifetime=lifetime,
-    ))
-    try:
-        users = resp.json()['response']['users']
-    except:
-        sys.stderr.write(resp.text)
-        raise
-
-
 def vk_group_members(group_id, count=1000, fields=()):
     """
     https://vk.com/dev/groups.getMembers
